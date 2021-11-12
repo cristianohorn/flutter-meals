42c0925e5bd45511e348116b17fa9256587a692d
Cristiano Horn <cristianohorn@gmail.com>
2021-11-11 21:47:19 -0300

    first commit with router/navigate

---
+++ src/meals/lib/components/category_item.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import '../models/category.dart';
+import '../screens/categories_meals_screen.dart';
+
+class CategoryItem extends StatelessWidget {
+  CategoryItem(Category this.category, {Key? key}) : super(key: key);
+
+  final Category category;
+
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (_) {
+        return CategoriesMealsScreen(category: category);
+      },
+    ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => _selectCategory(context),
+      borderRadius: BorderRadius.circular(15),
+      splashColor: Theme.of(context).primaryColor,
+      child: Container(
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        padding: EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(colors: [
+            category.color.withOpacity(.5),
+            category.color,
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+        ),
+      ),
+    );
+  }
+}
---
+++ src/meals/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import '../screens/categories_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.pink,
+        ).copyWith(
+          secondary: Colors.amber,
+        ),
+        fontFamily: 'Raleway',
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        textTheme: ThemeData.light().textTheme.copyWith(
+              headline6: const TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+              ),
+            ),
+      ),
+      home: CategoriesScreen(),
+    );
+  }
+}
